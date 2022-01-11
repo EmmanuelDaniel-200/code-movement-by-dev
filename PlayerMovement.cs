972a40129073e1e2307ae0ed7d08c368f4e86669
EmmanuelDaniel-200 <97138602+EmmanuelDaniel-200@users.noreply.github.com>
2022-01-11 15:58:36 +0300

    Add files via upload

---
+++ PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //from Emmanuel
+    //make sure that rigidbidy2D gravityscale = 0
+    public Rigidbody2D rb;
+    public float speed;
+    public float minY, maxY;
+    Vector2 newPos;
+
+
+    void Start(){
+
+    }
+
+    void FixedUpdate(){
+
+       
+        
+        
+        
+            float dir = Input.GetAxis("Vertical");
+            rb.velocity = Vector2.up * dir * speed;
+    }
+
+   
+}
